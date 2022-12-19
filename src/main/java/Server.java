2b814d1fa986749e47f54d497d5facddcfccf81e
Sridevi <sridevi.elangovan@gcitsolutions.com>
2022-12-19 11:04:08 +0530

    MultiThreadedWebServer Program

---
+++ src/main/java/Server.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+        System.out.println(Arrays.asList(args).toString());
+        try {
+
+            Properties properties = new Properties();
+            FileReader fr = new FileReader("src/main/resources/application.properties");
+
+
+            properties.load(fr);
+            int number = Integer.parseInt(properties.getProperty("server.port"));
+            ServerSocket server = new ServerSocket(number);
+            int counter = 0;
+            System.out.println("Server Started ....");
+            while (true) {
+                counter++;
+                Socket serverClient = server.accept();
+                System.out.println(" >> " + "Client No:" + counter + " started!");
+                ServerThread sct = new ServerThread(serverClient, counter);
+                sct.start();
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+}
---
+++ src/main/java/ServerThread.java
@@ -0,0 +1,45 @@
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class ServerThread extends Thread{
+    Socket serverClient;
+    int clientNo;
+
+    ServerThread(Socket inSocket,int counter){
+        serverClient = inSocket;
+        clientNo=counter;
+    }
+    public void run(){
+        try{
+
+            OutputStream os = serverClient.getOutputStream();
+            os.write("HTTP/1.0 200 OK\r\n".getBytes());
+//            os.write("Date: Fri, 31 Dec 1999 23:59:59 GMT\r\n".getBytes());
+//            os.write("Server: Apache/0.8.4\r\n".getBytes());
+//            os.write("Content-Type: text/html\r\n".getBytes());
+
+//            os.write("Expires: Sat, 01 Jan 2000 00:59:59 GMT\r\n".getBytes());
+//            os.write("Last-modified: Fri, 09 Aug 1996 14:21:40 GMT\r\n".getBytes());
+//            os.write("\r\n".getBytes());
+//            os.write("<TITLE>CSWeb</TITLE>".getBytes());
+            InputStream input = Files.newInputStream(Path.of("D:\\csweb\\index.html"));
+            ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+            byte[] data = new byte[1024];
+            int totRead = 0,numRead;
+            while ((numRead = input.read(data)) != -1) {
+                totRead += numRead;
+                os.write(data,0, numRead);
+            }
+//            os.write(("Content-Length: " + totRead + "\r\n").getBytes());
+            serverClient.close();
+        }catch(Exception ex){
+            System.out.println(ex);
+        }finally{
+            System.out.println("Client -" + clientNo + " exit!! ");
+        }
+    }
+}
